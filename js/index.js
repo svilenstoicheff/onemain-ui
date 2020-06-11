dbd9682b1831d9fc086901d55ecab15ab56c19eb
Svilen Stoicheff <svilen@yahoo.com>
2020-06-10 20:35:34 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// document.querySelectorAll('label.text').forEach((element) =>  {
+//   //  console.log(element);
+// element.addEventListener('click', (e) => {
+// console.log(e.target);
+//     document.querySelectorAll('.showBorder').forEach((element) => {
+//         element.classList.remove('showBorder');
+//     });
+//     e.target.querySelector('.noBorder').classList.add('showBorder');
+
+// })}
+// );
