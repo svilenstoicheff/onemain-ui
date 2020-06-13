72dd296ea4d68db3617e1e0c9aa49df208a43825
Svilen Stoicheff <svilen@yahoo.com>
2020-06-13 10:57:06 -0500

    Refactoring and cleanup

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 const errorMessages = {
-    loanAccountNumber: 'Loan Account Number is required', 
+    loanAccountNumber: 'Loan Account Number is required',
     routingNumber: 'Routing Number is required',
-    bankAccountNumber: 'Bank Account Number is required', 
+    bankAccountNumber: 'Bank Account Number is required',
     confirmBankAccountNumber: 'Confirm Bank Account Number is required',
-    numbersDontMatch: 'Bank Account numbers don\'t match', 
-    cardNumber: 'Card Number is required', 
-    nameOnCard: 'Name on card is required', 
-    expirationDate: 'Expiration Date is required', 
-    cvvNumber: 'CVV Number is required', 
+    numbersDontMatch: 'Bank Account numbers don\'t match',
+    cardNumber: 'Card Number is required',
+    nameOnCard: 'Name on card is required',
+    expirationDate: 'Expiration Date is required',
+    cvvNumber: 'CVV Number is required',
     cvvNumber3: 'CVV number has 3 digits',
-    numbersOnly: 'This field should contain numbers only', 
+    numbersOnly: 'This field should contain numbers only',
     routingNumber9: 'The routing number cannot be longer than 9 digits',
     numbersDontMatch: 'The bank account numbers don\'t match'
 }
 
-function attachEvents () {
+function attachEvents() {
     document.querySelector('#paymentDebit').addEventListener('click', e => {
         removeErrors();
         document.querySelector('#loanPayment').name = 'debit';
@@ -48,33 +48,42 @@ function attachEvents () {
 }
 
 function removeErrors() {
-    document.querySelectorAll('.error').forEach(el => el.remove());
+    document.querySelectorAll('div.error').forEach(el => el.remove());
+    document.querySelectorAll('h4.error').forEach(el => el.classList.remove('error'));
 }
 
-function validateChecking (e) {
+function validateChecking(e) {
     removeErrors();
     let formReady = false;
     const inputs = ['loanAccountNumber', 'routingNumber', 'bankAccountNumber', 'confirmBankAccountNumber'];
+
     inputs.forEach(el => {
         formReady = false;
-        let input = document.querySelector('input[name='+ el +'');
-        let error = document.createElement('div');
+        let input = document.querySelector('input[name=' + el + ''),
+            error = document.createElement('div'),
+            parent = input.parentElement,
+            heading4 = parent.querySelector('h4'),
+            inputValue = input.value.trim().replace(/ /g, '');
+
         error.classList.add('error');
-        let inputValue = input.value.trim().replace(/ /g,'');
-        
-        if( inputValue === '') {
+
+        if (inputValue === '') {
             error.innerText = errorMessages[el];
-            input.parentElement.after(error);
+            parent.after(error);
+            heading4.classList.add('error');
         } else if (!Number.isInteger(Number(inputValue))) {
             error.innerText = errorMessages['numbersOnly'];
-            input.parentElement.after(error);
-        } else if(el === 'routingNumber' && inputValue.length > 9) {
+            parent.after(error);
+            heading4.classList.add('error');
+        } else if (el === 'routingNumber' && inputValue.length > 9) {
             error.innerText = errorMessages['routingNumber9'];
-            input.parentElement.after(error);
-        } else if (el === 'confirmBankAccountNumber' && 
+            parent.after(error);
+            heading4.classList.add('error');
+        } else if (el === 'confirmBankAccountNumber' &&
             inputValue !== document.querySelector('[name=bankAccountNumber]').value) {
             error.innerText = errorMessages['numbersDontMatch'];
-            input.parentElement.after(error);
+            parent.after(error);
+            heading4.classList.add('error');
         } else {
             formReady = true;
         }
@@ -82,33 +91,41 @@ function validateChecking (e) {
     return formReady;
 }
 
-function validateDebit (e) {
+function validateDebit(e) {
     removeErrors();
+
     let formReady = false;
     const inputs = ['loanAccountNumber', 'cardNumber', 'nameOnCard', 'expirationDate', 'cvvNumber'];
     inputs.forEach(el => {
         formReady = false;
-        let input = document.querySelector('input[name='+ el +'');
-        let error = document.createElement('div');
+        let input = document.querySelector('input[name=' + el + ''),
+            error = document.createElement('div'),
+            parent = input.parentElement,
+            heading4 = parent.querySelector('h4'),
+            inputValue = input.value.trim().replace(/ /g, '');
+
         error.classList.add('error');
-        let inputValue = input.value.trim().replace(/ /g,'');
+
         input.classList.remove('hidden');
-        if(inputValue === '') {
+        if (inputValue === '') {
             error.innerText = errorMessages[el];
 
-            if(el === 'cvvNumber' || el === 'expirationDate') {
-                input.parentElement.after(error);
+            if (el === 'cvvNumber' || el === 'expirationDate') {
+                parent.after(error);
+                heading4.classList.add('error');
             } else {
-                input.parentElement.after(error);
+                parent.after(error);
+                heading4.classList.add('error');
             }
 
-
-        } else if (el !== 'nameOnCard' && !Number.isInteger(Number(inputValue))) {
+        } else if (el !== 'nameOnCard' && el !== 'expirationDate' && !Number.isInteger(Number(inputValue))) {
             error.innerText = errorMessages['numbersOnly'];
-            input.parentElement.after(error);
-        } else if(el === 'cvvNumber' && inputValue.length < 3) {
+            parent.after(error);
+            heading4.classList.add('error');
+        } else if (el === 'cvvNumber' && inputValue.length < 3) {
             error.innerText = errorMessages['cvvNumber3'];
-            input.parentElement.after(error);
+            parent.after(error);
+            heading4.classList.add('error');
         } else {
             formReady = true;
         }
@@ -119,10 +136,8 @@ function validateDebit (e) {
 function validateForm(e) {
     e.preventDefault();
     const formName = e.target.name;
-    let formReady = formName === 'checking'? validateChecking(e) : validateDebit(e);
-    console.log('formReady', formReady);
-    if(formReady) e.target.submit();
+    let formReady = formName === 'checking' ? validateChecking(e) : validateDebit(e);
+    if (formReady) e.target.submit();
 }
 
 window.addEventListener('DOMContentLoaded', attachEvents);
-//document.querySelector('body').addEventListener('load', attachEvents);
